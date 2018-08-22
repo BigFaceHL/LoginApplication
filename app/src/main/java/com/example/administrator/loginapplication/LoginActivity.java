32f0bd412ecbbd4a865a8764b52db18d862ee5c2
helan <1286743738@qq.com>
2018-08-22 17:23:06 +0800

    第一次commit

--- app/src/main/java/com/example/administrator/loginapplication/LoginActivity.java
+++ app/src/main/java/com/example/administrator/loginapplication/LoginActivity.java
@@ -62,7 +62,7 @@ public class LoginActivity extends AppCompatActivity {
                 login(etUserName.getText().toString().trim(), etPassword.getText().toString().trim());
                 Result result=new Result("200",new User("001","helan"));
                 String json=new GsonBuilder().create().toJson(result,Result.class);
-                Log.e("123",json);
+                Log.e("1239",json);
 
 
             }
